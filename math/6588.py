5a26754be56557bf5873190b1b635ce3b95ba6e4
jongwoo <jongwoo@atommerce.com>
2020-10-06 10:06:18 +0900

    201006

--- math/6588.py
+++ math/6588.py
@@ -1,2 +1,24 @@
-# https://www.acmicpc.net/problem/6588
+import sys
 
+arr = [_ for _ in range(1000001)]
+check = [True for _ in range(1000001)]
+check[0] = False
+check[1] = False
+num = 1000000
+for i in range(2, int(num ** 0.5)+1):
+    if check[i] == True:
+        for j in range(i+i, num, i):
+            check[j] = False
+
+input = sys.stdin.readline
+
+
+while True:
+    input_num = int(input())
+    if input_num == 0:
+        break
+    for i in range(3, num):
+        if check[i] == True:
+            if check[input_num-i] == True:
+                print("%d = %d + %d" % (input_num, i, input_num-i))
+                break
