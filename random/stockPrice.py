c60bed2a58f18bca1658225d146eed2f6ba4e4af
oowgnoj <mangtaob@gmail.com>
2020-12-15 09:53:18 +0900

    201215-2

--- random/stockPrice.py
+++ random/stockPrice.py
@@ -1,29 +1,46 @@
-import collections
-import copy
+# import collections
+# import copy
 
+
+# def solution(prices):
+#     answer = []
+#     queue = collections.deque(prices)
+#     while queue:
+#         current_value = queue.popleft()
+#         current_queue = copy.copy(queue)
+#         i = 0
+#         while True:
+#             if not current_queue:
+#                 answer.append(i)
+#                 break
+#             i += 1
+#             compare_value = current_queue.popleft()
+#             if compare_value >= current_value:
+#                 continue
+#             else:
+#                 answer.append(i)
+#                 break
+
+#     return answer
+
+
+# 효율성 - 시간초과
 
 def solution(prices):
     answer = []
-    queue = collections.deque(prices)
-    while queue:
-        current_value = queue.popleft()
-        current_queue = copy.copy(queue)
-        i = 0
-        while True:
-            if not current_queue:
-                answer.append(i)
-                break
-            i += 1
-            compare_value = current_queue.popleft()
-            if compare_value >= current_value:
-                continue
-            else:
-                answer.append(i)
+    for i, val in enumerate(prices):
+        cnt = 0
+        if len(prices)-1 == i:
+            answer.append(cnt)
+            break
+        for j, compare in enumerate(prices[i+1:]):
+            cnt += 1
+            if compare < val or j == len(prices[i+1:])-1:
+                print('here', cnt)
+                answer.append(cnt)
                 break
 
     return answer
 
-
-# 효율성 - 시간초과
 
 print(solution([1, 2, 3, 2, 3]))
